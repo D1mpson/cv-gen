ecceda2cab18f8fad4d98de11ff67e6618eb47b1
D1mpson <165700584+D1mpson@users.noreply.github.com>
2025-05-15 10:42:05 +0300

    4.2

--- src/main/java/com/example/cvgenerator/model/User.java
+++ src/main/java/com/example/cvgenerator/model/User.java
@@ -47,6 +47,7 @@ public class User {
     private LocalDate birthDate;
 
     @NotNull(message = "Місто є обовʼязковим")
+    @Column(name = "city_life") // Додаємо анотацію з явним іменем колонки
     private String cityLife = ""; // Додаємо значення за замовчуванням
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
