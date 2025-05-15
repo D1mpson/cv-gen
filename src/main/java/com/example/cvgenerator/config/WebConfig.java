40310d8ba988c7330424a796c3aa0e926db713ea
D1mpson <165700584+D1mpson@users.noreply.github.com>
2025-05-15 14:24:30 +0300

    Test 3

--- src/main/java/com/example/cvgenerator/config/WebConfig.java
+++ src/main/java/com/example/cvgenerator/config/WebConfig.java
@@ -1,19 +1,16 @@
-package com.example.cvgenerator.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-
-import java.util.Locale;
-
-@Configuration
-public class WebConfig {
-
-    @Bean
-    public LocaleResolver localeResolver() {
-        SessionLocaleResolver slr = new SessionLocaleResolver();
-        slr.setDefaultLocale(new Locale("uk", "UA"));
-        return slr;
-    }
-}
+//package com.example.cvgenerator.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//    // Видаляємо метод localeResolver()
+//
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/uploads/**")
+//                .addResourceLocations("file:uploads/");
+//    }
+//}
